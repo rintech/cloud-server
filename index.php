7702e1ba3e0a7bfbb61a29775d2962bcd3152a3c
Crzywolf <hbk8668@gmail.com>
2018-05-10 00:10:00 +0900

    index.php 誰もができるゴリラのような書き方

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 
 ?>
-<!-- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="content-language" content="ja">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>cloud-server</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-
 <body>
 
 </body>
-
-</html> -->
+</html>
